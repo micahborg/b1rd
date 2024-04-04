2b4c24f393b4aebbb808ec56e273afa94ca17d2c
Micah Borghese <micahborghese@gmail.com>
2024-04-04 00:10:20 -0500

    homepage commit, logo upload, navbar finished

--- index.php
+++ index.php
@@ -1,5 +1,70 @@
-<html>
-    <?php
-        echo "Hello World!";
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>B1RD: Online Bird Adoption Platform</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body> 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">B1RD</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Discover</a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="text" placeholder="Username" aria-label="Username" style="padding-left: 10px; padding-right: 10px;">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Login</button>
+            </form>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Welcome to B1RD</h1>
+                <p>Find your perfect bird companion today!</p>
+            </div>
+        </div>
+        <div class="row" style="justify-content: center;">
+            <div class="col">
+                <h2>Adopt</h2>
+                <p>Find your perfect bird companion today!</p>
+                <a href="#" class="btn btn-primary">Adopt Now</a>
+            </div>
+            <div class="col">
+                <h2>Discover</h2>
+                <p>Discover the perfect bird for you!</p>
+                <a href="#" class="btn btn-primary">Discover Now</a>
+            </div>
+            <div class="col">
+                <h2>Learn</h2>
+                <p>Learn more about birds and how to care for them!</p>
+                <a href="#" class="btn btn-primary">Learn More</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+<footer>
+    <div class="container">
+        <p>&copy; 2024 B1RD. Made with 🔥 by Code M.</p>
+    </div>
+</footer>
 </html>
